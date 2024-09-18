03c7904d1c637ce41839d3ad73846506241d05f0
Tom <tomek286@yahoo.com>
2024-09-18 12:55:34 +0200

    security

---
+++ src/main/java/com/shopcollection/shopcollection/security/user/ShopUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.shopcollection.shopcollection.security.user;
+
+import com.shopcollection.shopcollection.model.User;
+import com.shopcollection.shopcollection.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
